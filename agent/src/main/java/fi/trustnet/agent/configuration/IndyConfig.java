77ebeacb45b34a0f9821ae93eff7ee3fb4a3c858
astrozombie <31222577+astrozombie@users.noreply.github.com>
2018-04-04 15:06:23 +0300

    moving wallet_name

--- agent/src/main/java/fi/trustnet/agent/configuration/IndyConfig.java
+++ agent/src/main/java/fi/trustnet/agent/configuration/IndyConfig.java
@@ -4,4 +4,5 @@ public class IndyConfig {
     public static final String USER_DID = "VJkTdbQZTRnJneZAR245n6";
     public static final String ISSUERSEED = "000000000000000000000000000Alice";
     public static final String NETWORK_NAME = "default_pool";
+    public static final String USER_WALLET_NAME = "alice";
 }
