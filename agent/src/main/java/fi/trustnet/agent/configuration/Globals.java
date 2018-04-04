31b8a19b7e855148bce05ced10a808aef52f2037
astrozombie <31222577+astrozombie@users.noreply.github.com>
2018-04-04 15:05:56 +0300

    moving wallet_name to indyconfig

--- agent/src/main/java/fi/trustnet/agent/configuration/Globals.java
+++ agent/src/main/java/fi/trustnet/agent/configuration/Globals.java
@@ -2,5 +2,4 @@ package fi.trustnet.agent.configuration;
 
 public class Globals {
     public static final String DID_AUTH_HEADER = "didauth";
-    public static final String USER_WALLET_NAME = "alice";
 }
